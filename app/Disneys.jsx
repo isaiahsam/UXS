a0b97bb38afee756cd1882b72780351e8a0abdc9
isaiahsam <isaiahpascual12@gmail.com>
2023-11-20 23:23:24 +0800

    border card

--- app/Disneys.jsx
+++ app/Disneys.jsx
@@ -36,26 +36,28 @@ import Image from "next/image";
 export default function Disneys({ disneys }) {
     return (
         <>
-            {/* <ul className="grid grid-cols-4 mx-auto max-w-[1260px] gap-10"> */}
             <ul className="grid grid-cols-1 mx-auto max-w-[1260px] gap-20">
                 {disneys.map((disney) => (
-                    <li key={disney.movieID} className="flex flex-col items-center mb-4">
+                    <li key={disney.movieID} className="flex flex-col items-center mb-4 border border-white p-4">
                         <div className="flex items-center justify-center">
                             <Image
                                 src={disney.image}
                                 alt={disney.title}
-                                width={70}
-                                height={70}
+                                width={300}
+                                height={300}
                             />
                         </div>
-                        <p>{disney.title}</p>
-                        {/* Additional information */}
-                        <p>{disney.summary}</p>
+                        <div className="mt-4">
+                            <p className="text-xl font-bold">{disney.title}</p>
+                            {/* Additional information */}
+                            <p className="text-gray-500">{disney.summary}</p>
+                        </div>
                     </li>
                 ))}
             </ul>
         </>
     );
 }
+
 
 
